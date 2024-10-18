9cb3246165006517bc7693a51e663a4fa03976b0
B.Sravani <sravani@napqueens.com>
2024-10-18 21:18:15 +0530

    added print statement

--- NQ_S3.py
+++ NQ_S3.py
@@ -345,7 +345,7 @@ def load_and_preprocess_data():
     merged_data_df = pd.merge(df_scrapped_cleaned, merged_data_df[['asin', 'brand', 'product_title', 'price', 'date']], left_on='ASIN', right_on='asin', how='left')
     #st.write("Merged df_scrapped_cleaned with merged_data_df:", merged_data_df.head())
     # Debugging: Check merged_data_df after renaming and modifying 'asin'
-    #st.write("Loaded merged_data_df with latest date (dynamic):", merged_data_df.head())
+    st.write("Loaded merged_data_df with latest date (dynamic):", merged_data_df.head())
     
     price_data_df = load_latest_csv_from_s3('napqueen_price_tracker')
     #st.write("Loaded price_data_df (napqueen_price_tracker):", price_data_df.head())

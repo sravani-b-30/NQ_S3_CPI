bbd8941dbdbdec067b1c1ccdbc9fa9c5eb66a3d0
sravani-b-30 <sravani@napqueens.com>
2025-03-19 17:02:37 +0530

    removed aws credentials necessity in the script

--- Walmart_DataPipeline/src/s3_utils.py
+++ Walmart_DataPipeline/src/s3_utils.py
@@ -7,9 +7,9 @@ import io
 
 # Initialize S3 client
 s3_client = boto3.client(
-    's3',
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+    's3'
+    # aws_access_key_id=AWS_ACCESS_KEY_ID,
+    # aws_secret_access_key=AWS_SECRET_ACCESS_KEY
 )
 
 def fetch_product_details_from_s3(bucket_name, s3_folder, file_name):
